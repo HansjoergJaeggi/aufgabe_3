1bf54d25747b9dcb81c855c43f3fe5dbec3cbe08
HansjoergJaeggi <68072170+HansjoergJaeggi@users.noreply.github.com>
2022-06-06 21:01:30 +0200

    Kommentare hinzugefügt

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
 let Bild = 0
 let Zufallszahl = 0
+/**
+ * Aufgabe 3
+ * 
+ * Aus drei verschiedenen Symbolen sollte eines zufällig ausgewählt werden. Dieses Symbol sollte danach zufällig oft blinken. 
+ * 
+ * Frage: Wieviele Variablen brauchen wir?
+ * 
+ * Wie wählen wir zufällig ein Symbol?
+ * 
+ * Wie stellen wir sicher, dass zufällig häufig dieses Symbol dann geblinkt wird?
+ */
+/**
+ * Arrey mit 5*5 dunkeln LED zur Erzeugung der  Blink-Pausen von 200ms
+ */
+/**
+ * Arrey mit 5*5 dunkeln LED zur Erzeugung der  Blink-Pausen von 200ms
+ */
 basic.forever(function () {
     Bild = randint(1, 3)
     Zufallszahl = randint(1, 4)
